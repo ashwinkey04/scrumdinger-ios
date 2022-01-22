8892a1adaad7b8ee8a8538af67093b6aecc831fc
Ashwin Ramakrishnan <ashwinkey04@gmail.com>
2022-01-22 20:27:46 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ashwin Ramakrishnan on 22/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
